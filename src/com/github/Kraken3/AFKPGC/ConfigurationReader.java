1e5089172bff909abfde1edd3b2b4c680a87b3dd
Daniel Boston <programmerdan@gmail.com>
2015-07-22 03:29:10 -0400

    Few more fixes to make config loading work.
    Note CivTest is reporting Server.getMaxPlayers() of 500? so updated config file to appropriate values.
    
    Note:
        int a = 9;
    	String b = "I'm " + a + 1 + " years old";
    
    returns ```I'm 91 years old```.
    
        String c = "I'm " + (a + 1) + " years old";
    
    returns ```I'm 10 years old```. Be sure to enclose math during concatenation with parenthesis. Remember these single line constructs are turned into StringBuilder expressions with each ```+``` converted to an ```.append(...)```.
    
    When iterating do your best to avoid generating a log output every execution of the loop in worst case.
    
    Use regex, versus your own custom pattern matcher, or java.util.Scanner for well defined token sets.

--- src/com/github/Kraken3/AFKPGC/ConfigurationReader.java
+++ src/com/github/Kraken3/AFKPGC/ConfigurationReader.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -18,6 +20,8 @@ public class ConfigurationReader {
 		FileConfiguration conf = AFKPGC.plugin.getConfig();
 
 		int max_players = AFKPGC.plugin.getServer().getMaxPlayers();
+		AFKPGC.logger.info("Server reports maximum of " + max_players + " players");
+
 		int[] kickThresholds = new int[max_players];
 		for (int i = 0; i < max_players; i++)
 			kickThresholds[i] = -1;
@@ -28,6 +32,7 @@ public class ConfigurationReader {
 			nums[0] = nums[1] = nums[2] = -1;
 			parseNaturals(s, nums);
 			int min = nums[0], max = nums[1], t = nums[2];
+			AFKPGC.logger.info("Between " + min + " and " + max + " kick after " + t + " seconds.");
 			if (min > max || min < 1 || max < 1 || t < 0) {
 				AFKPGC.logger.warning("Configuration file error: " + s);
 				return false;
@@ -39,20 +44,42 @@ public class ConfigurationReader {
 						AFKPGC.logger.config("Previously defined threshold getting redefined in: "
 										+ s);
 					kickThresholds[i - 1] = t;
+				} else {
+					break; // don't keep going if we're not gonna use it.
 				}
 			}
 		}
 
 		boolean foundEmptyThreshold = false;
+		int gapStart = -1;
+		int gapEnd = -1;
 		for (int i = 0; i < max_players; i++) {
 			if (kickThresholds[i] == -1) {
-				AFKPGC.logger.warning("Configuration file incomplete - plugin doesn't know when to kick players when there are "
-										+ i + 1 + " players online");
+				if (gapStart > -1) {
+					gapEnd = i;
+				} else {
+					gapStart = i;
+					gapEnd = i;
+				}
 				foundEmptyThreshold = true;
+			} else if (gapStart > -1) {
+				AFKPGC.logger.warning("Configuration file incomplete - plugin doesn't know when to kick players when there are between "
+						+ (gapStart + 1) + " and "
+						+ (gapEnd + 1) + " players online");
+				gapStart = -1;
+				gapEnd = -1;
 			}
+
 		}
-		if (foundEmptyThreshold)
+
+		if (foundEmptyThreshold) {
+			if (gapStart > -1) {
+				AFKPGC.logger.warning("Configuration file incomplete - plugin doesn't know when to kick players when there are between "
+						+ (gapStart + 1) + " and "
+						+ (gapEnd + 1) + " players online");
+			}
 			return false;
+		}
 
 		ArrayList<Warning> warnings = new ArrayList<Warning>();
 		ktl = conf.getStringList("warnings");
@@ -106,25 +133,22 @@ public class ConfigurationReader {
 		return true;
 	}
 
+	/**
+	 * attempt to fill numbers but quietly fail.
+	 */
 	public static void parseNaturals(String str, int[] numbers) {
-		int len = str.length();
-		int numberIndex = 0;
-		int maxIndex = numbers.length;
-		int currentNumber = 0;
-		boolean numberStarted = false;
-
-		for (int i = 0; i < len; i++) {
-			char c = str.charAt(i);
-			if (c >= '0' && c <= '9') {
-				numberStarted = true;
-				currentNumber = currentNumber * 10 + (c - '0');
-				numbers[numberIndex] = currentNumber;
-			} else if (numberStarted) {
-				numberStarted = false;
-				currentNumber = 0;
-				numberIndex++;
-				if (numberIndex == maxIndex)
-					return;
+		Pattern num = Pattern.compile("[0-9]+");
+		Matcher findnum = num.matcher(str);
+		for (int i = 0; i < numbers.length; i++) {
+			if (findnum.find()) {
+				String cmatch = findnum.group();
+				try {
+					numbers[i] = Integer.valueOf(cmatch);
+				} catch (NumberFormatException nfe) {
+					break;
+				}
+			} else {
+				break;
 			}
 		}
 	}
