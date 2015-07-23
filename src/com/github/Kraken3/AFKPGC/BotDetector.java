a4b415b4d21f6e9a28768c9a2ab3db93825f95cf
Daniel Boston <programmerdan@gmail.com>
2015-07-23 01:17:42 -0400

    Adding in bounding box framing code
    Fleshing out better bound detection, then on to true bot detection.
    
    Realized I could use the top offenders list to pick the first offender to fail my truebot detection, and not mess with a collection after that. Hurray.

--- src/com/github/Kraken3/AFKPGC/BotDetector.java
+++ src/com/github/Kraken3/AFKPGC/BotDetector.java
@@ -11,6 +11,8 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.TreeMap;
+import java.util.HashMap;
 
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
@@ -18,163 +20,188 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 /*
+ * Detects likely bots based on movement patterns
  * @author Maxopoly
+ * @author ProgrammerDan
+ * 
  */
 public class BotDetector implements Runnable {
 	public static boolean longBans;
 	public static float currentTPS = 20;
 	public static float acceptableTPS;
 	public static float criticalTPSChange;
+	public static float relaxationFactor; // TODO
+	public static int maxLocations; // TODO
+	public static int maxSuspects; // TODO
+	public static int maxReprieve; // TODO
+	public static int minBaselineMovement; // TODO
+	public static int shortBan; //TODO in milliseconds
+	public static int longBan; //TODO in milliseconds
+	public static BoundResultsConfiguration boundsConfig; // TODO
 	public static long frequency; // how often this runs in ticks
 	public static File banfile;
 	float lastRoundTPS;
-	UUID topSuspect = null;
-	String topSuspectName = ""; // needs to be stored additionally,
-								// because retrieving the name off
-								// players after kicking them is only
-								// possible
-								// over deprecated/buggy methods
-	Location topSuspectsLocation = null;
-	UUID lastRoundSuspect = null;
-	String lastRoundSuspectName = "";
-	Location lastRoundSuspectsLocation = null;
-	public static LinkedList<String> suspectedBotters = new LinkedList<String>();
-	public static LinkedList<String> bannedPlayers = new LinkedList<String>();
-	// this is needed as a separated list, so we know the difference between
-	// players who were banned by AFKPGC and players who were banned for other
-	// reasons
 
-	static BanList banList = AFKPGC.plugin.getServer().getBanList(
-			BanList.Type.NAME); // ban after names not ips
+	TreeMap<Integer, Suspect> topSuspects;
+	HashMap<UUID, Integer> reprieve; // temp. cleared suspects
+	
+	Suspect lastRoundSuspect = null;
+	
+	public static LinkedList<String> suspectedBotters = new LinkedList<String>();
+	/* this is needed as a separated list, so we know the difference between players
+	 * who were banned by AFKPGC and players who were banned for other reasons */
+	public static LinkedList<String> bannedPlayers = new LinkedList<String>();
+
+	// ban after names not ips
+	static BanList banList = AFKPGC.plugin.getServer().getBanList(BanList.Type.NAME);
 
 	public void run() {
+		if (topSuspects == null) {
+			topSuspects = new TreeMap<Integer, Suspect>();
+		}
+		if (reprieve == null) {
+			reprieve = new HashMap<UUID, Integer>();
+		} else {
+			// decrement reprieve.
+			for (Iterator<Map.Entry<UUID, Integer>> i = reprieve.entrySet().iterator(); i.hasNext(); ) {
+				Map.Entry<UUID, Integer> entry = i.next();
+				Integer roundsLeft = entry.getValue() - 1;
+				if (roundsLeft <= 0) { // no more reprieve
+					i.remove();
+				} else {
+					entry.setValue(roundsLeft);
+				}
+			}
+		}
+
 		if (!AFKPGC.enabled) {
 			return;
 		}
 		currentTPS = TpsReader.getTPS();
 		Map<UUID, LastActivity> lastActivities = LastActivity.lastActivities;
 		if (currentTPS < acceptableTPS) {
-			int smallestMovedDistance = 1024;
-			Set<Map.Entry<UUID, LastActivity>> entries = lastActivities
-					.entrySet();
+			topSuspects.clear();
+			int smallestMovedDistance = minBaselineMovement;
+			Set<Map.Entry<UUID, LastActivity>> entries = lastActivities.entrySet();
+			// find new top suspects
 			for (Map.Entry<UUID, LastActivity> entry : entries) {
 				UUID playerUUID = entry.getKey();
-				if (lastActivities.containsKey(playerUUID)) { // according to
-																// the author of
-																// AFKGPC, there
-																// might be
-																// inconsistencies
-																// in this list,
-																// so this
-																// additional
-																// check is
-																// needed
+				/* according to the author of AFKGPC, there might be
+				 * inconsistencies in this list, so this additional
+				 * check is needed */
+				if (lastActivities.containsKey(playerUUID)) {
 					LastActivity la = entry.getValue();
-					la.loggedLocations.add(Bukkit.getPlayer(playerUUID)
-							.getLocation());
-					if (la.loggedLocations.size() >= 5) {
-						if (la.loggedLocations.size() > 5) {
+					la.loggedLocations.add(Bukkit.getPlayer(playerUUID).getLocation());
+					if (la.loggedLocations.size() >= maxLocations) {
+						if (la.loggedLocations.size() > maxLocations) {
 							la.loggedLocations.removeFirst();
 						}
-						int itWasntMeISwear = la.calculateMovementradius();
-						if (itWasntMeISwear < smallestMovedDistance) {
-							smallestMovedDistance = itWasntMeISwear;
-							Player dirtyLiar = Bukkit.getPlayer(playerUUID);
+						// we keep tracking location even if on reprieve.
+						if (!reprieve.containsKey(playerUUID)) {
+							int itWasntMeISwear = la.calculateMovementradius();
+							if (itWasntMeISwear < smallestMovedDistance) {
+								smallestMovedDistance = itWasntMeISwear;
+								Player dirtyLiar = Bukkit.getPlayer(playerUUID);
 
-							topSuspect = playerUUID;
-							topSuspectsLocation = dirtyLiar.getLocation();
-							topSuspectName = dirtyLiar.getName();
-						}
+								topSuspects.put(itWasntMeISwear, new Suspect(
+										playerUUID, dirtyLiar.getName(), dirtyLiar.getLocation(),
+										la.evaluateBounds(relaxationFactor) ) );
 
-					}
-
-				}
-			}
-			if (topSuspect != null) {
-				if (!AFKPGC.immuneAccounts.contains(topSuspect)) {
-					Date currentDate = new Date();
-					long bantime = (long) (frequency / currentTPS * 1000);
-					// Because the ban time is based on real time and the next
-					// run
-					// of this method is based on the tick, the ban time needs
-					// to be
-					// adjusted to the tick. The long form of this would be:
-					// (20/currentTPS) * (frequency/20) * 1000
-					// The time is in ms
-					banList.addBan(topSuspectName,
-							"You were suspected to cause lag and banned for "
-									+ bantime / 1000 + " seconds", new Date(
-									currentDate.getTime() + bantime), null);
-					// ban the player for ~ a minute
-				}
-			}
-			if (lastRoundSuspect != null) {
-				if (!AFKPGC.immuneAccounts.contains(lastRoundSuspect)) {
-					if (currentTPS - lastRoundTPS > criticalTPSChange) { // value
-																			// needs
-																			// to
-																			// be
-						// configured, that was
-						// just the first thing
-						// that came to mind.
-						// This can be
-						// relatively sensitive,
-						// because it will only
-						// ban players for a
-						// longer period of
-						// time, if it catches
-						// them twice here
-						Date currentDate = new Date();
-						if (suspectedBotters.contains(lastRoundSuspectName)) {
-							if (longBans) {
-								banList.addBan(
-										lastRoundSuspectName,
-										"Kicking you resulted in a noticeable TPS im"
-												+ "provement, so you were banned until the TPS goes"
-												+ " back to normal values.",
-										new Date(
-												currentDate.getTime() + 6 * 3600 * 1000),
-										null); // 6h ban
-								bannedPlayers.add(lastRoundSuspectName);
-								addToBanfile(lastRoundSuspectName);
-								suspectedBotters.remove(lastRoundSuspectName);
+								if (topSuspects.size() > maxSuspects) {
+									topSuspects.pollLastEntry(); // gets rid of largest distance
+								}
 							}
-							AFKPGC.logger.info("The player "
-													+ lastRoundSuspectName
-													+ " causes lag and is a repeated offender,"
-													+ " kicking him resulted in a TPS improvement of "
-													+ String.valueOf(currentTPS
-															- lastRoundTPS)
-													+ " at the location "
-													+ lastRoundSuspectsLocation
-															.toString());
+						}
+					}
+				}
+			}
+			Suspect thisRoundSuspect = null;
+			// Now find first top suspect to pass the truebot tests.
+			if (topSuspects.size() > 0) {
+				for (Map.Entry<Integer, Suspect> entry : topSuspects) {
+					Suspect curSuspect = entry.getValue();
+					if (!AFKPGC.immuneAccounts.contains(curSuspect.getUUID())) {
+						// Test Bounds for truebot(tm) detection.
+						BoundResult bounds = curSuspect.getResults();
+						if (bounds != null) {
+							double truebot = 0.0;
+							truebot += (bounds.getContained() ? boundsConfig.getContained() : 0.0);
+							truebot += (bounds.getContainedExcludeY() ? boundsConfig.getContainedExcludeY() : 0.0);
+							truebot += (bounds.getVolumeSimilar() ? boundsConfig.getVolumeSimilar() : 0.0);
+							truebot += (bounds.getSurfaceSimilar() ? boundsConfig.getSurfaceSimilar() : 0.0);
+							truebot += (bounds.getNearlyContained() ? boundsConfig.getNearlyContained() : 0.0);
+							truebot += (bounds.getNearlyContainedExcludeY() ? 
+									boundsConfig.getNearlyContainedExcludeY() : 0.0);
+
+							if (truebot >= boundsConfig.getThreshold()) {
+								// Its movement looks botlike.
+
+								// Now test surrounding area.
+								Location point = curSuspect.getLocation();
+
+								// TODO
+								{
+									thisRoundSuspect = curSuspect;
+									Date currentDate = new Date();
+									long bantime = (long) (frequency / currentTPS * 1000);
+									/* Because the ban time is based on real time and the next run
+									 * of this method is based on the tick, the ban time needs to be
+									 * adjusted to the tick. The long form of this would be:
+									 * (20/currentTPS) * (frequency/20) * 1000
+									 * The time is in ms */
+									banList.addBan(curSuspect.getName(),
+											"You were suspected to cause lag and banned for "
+													+ bantime / 1000 + " seconds",
+													new Date(currentDate.getTime() + bantime), null);
+									// ban the player briefly and skip the other suspects.
+									break;
+								}
+							} 
+							// It passed the truebot(tm) detection -- for now. Give a temporary reprieve.
+							reprieve.put(curSuspect.getUUID(), maxReprieve);
+						} // else not enough info yet. Pass.
+					}
+				}
+			}
+
+			if (lastRoundSuspect != null) {
+				if (!AFKPGC.immuneAccounts.contains(lastRoundSuspect.getUUID())) {
+					if (currentTPS - lastRoundTPS > criticalTPSChange) {
+						/* This can be relatively sensitive, because it will only ban players for 
+						 * a longer period of time, if it catches them twice here */
+						Date currentDate = new Date();
+						if (suspectedBotters.contains(lastRoundSuspect.getName())) {
+							if (longBans) {
+								banList.addBan( lastRoundSuspect.getName(),
+										"Kicking you resulted in a noticeable TPS improvement, so you " +
+										"were banned until the TPS goes back to normal values.",
+										new Date(currentDate.getTime() + longBan),
+										null); // long ban.
+								bannedPlayers.add(lastRoundSuspect.getName());
+								addToBanfile(lastRoundSuspect.getName());
+								suspectedBotters.remove(lastRoundSuspect.getName());
+							}
+							AFKPGC.logger.info("The player " + lastRoundSuspect.getName()
+									+ " causes lag and is a repeated offender, kicking him resulted"
+									+ " in a TPS improvement of " + String.valueOf(currentTPS - lastRoundTPS)
+									+ " at the location " + lastRoundSuspect.getLocation().toString());
 
 						} else {
-							suspectedBotters.add(lastRoundSuspectName);
-							AFKPGC.logger.info( "The player "
-													+ lastRoundSuspectName
-													+ " is suspected to cause lag,"
-													+ " kicking him resulted in a TPS improvement of "
-													+ String.valueOf(currentTPS
-															- lastRoundTPS)
-													+ " at the location "
-													+ lastRoundSuspectsLocation
-															.toString());
+							suspectedBotters.add(lastRoundSuspect.getName());
+							AFKPGC.logger.info( "The player " + lastRoundSuspect.getName()
+									+ " is suspected to cause lag, kicking him resulted in a TPS improvement of "
+									+ String.valueOf(currentTPS - lastRoundTPS) + " at the location "
+									+ lastRoundSuspects.getLocation().toString());
 						}
 					}
 				}
 			}
 
 			lastRoundTPS = currentTPS;
-			lastRoundSuspect = topSuspect;
-			lastRoundSuspectName = topSuspectName;
-			lastRoundSuspectsLocation = topSuspectsLocation; // as preparation
-																// for the next
-																// run;
+			lastRoundSuspect = thisRoundSuspect;
 		} else { // TPS is high enough
 			if (bannedPlayers.size() != 0) {
-				freeEveryone(); // not everyone, but everyone banned by this
-								// plugin
+				freeEveryone(); // not everyone, but everyone banned by this plugin
 			}
 		}
 
--- src/com/github/Kraken3/AFKPGC/LastActivity.java
+++ src/com/github/Kraken3/AFKPGC/LastActivity.java
@@ -17,50 +17,75 @@ public class LastActivity{
 	public static long currentTime; 	//OCD compels me to save a few System.currentTimeMillis() calls..	
 	public LinkedList <Location> loggedLocations;
 	public long timeOfLastActivity;
-	public long timeOflastKickerPass; //time of the last Kicker.run call, relevant for warnings
+	public long timeOfLastKickerPass; //time of the last Kicker.run call, relevant for warnings
 	public UUID playerName; //useful only in onCommandList
+	public ActivityBounds lastBounds; 
 	
 	//let's be polite.. I strongly dislike bukkit.. onPlayerQuitEvent doesn't trigger on all
 	//player log off events for some reason. This causes LastActivity.lastActivities to contain more players
 	//than there are playing on the server. FixInconsitencies() fixes this problem.
 	//It's ok. We are in agreement with our hatred.
-	static public void FixInconsitencies(){
-		Map<UUID, LastActivity> lastActivities = LastActivity.lastActivities;	
+	static public void FixInconsistencies(){
+		Map<UUID, LastActivity> lastActivities = LastActivity.lastActivities;
 		Player[] players = new Player[AFKPGC.plugin.getServer().getOnlinePlayers().size()];
 		int x = 0;
-		for (Player p: AFKPGC.plugin.getServer().getOnlinePlayers()){
+		for (Player p: AFKPGC.plugin.getServer().getOnlinePlayers()) {
 			players[x] = p;
 			x++;
 		}
-		TreeSet<UUID> playersTree = new TreeSet<UUID>();		
+		TreeSet<UUID> playersTree = new TreeSet<UUID>();
 		
-		for(Player p:players) {
+		for (Player p:players) {
 			UUID uuid = p.getUniqueId();
 			LastActivity la = lastActivities.get(uuid);
-			if(la == null) {
+			if (la == null) {
 				la = AFKPGC.addPlayer(p);
 			}
-			if(la != null) {
-				la.timeOflastKickerPass = LastActivity.currentTime;
+			if (la != null) {
+				la.timeOfLastKickerPass = LastActivity.currentTime;
 				playersTree.add(uuid);
 			}
-		}				
+		}
 		
-		UUID[] keySet = lastActivities.keySet().toArray(new UUID[0]);		   
-		for(UUID i:keySet){
-			if(!playersTree.contains(i)) AFKPGC.removerPlayer(i);			   
+		UUID[] keySet = lastActivities.keySet().toArray(new UUID[0]);
+		for (UUID i:keySet) {
+			if (!playersTree.contains(i)) {
+				AFKPGC.removerPlayer(i);
+			}
 		}		
 		
 	}
+
 	public int calculateMovementradius() {
-		Location current=loggedLocations.getLast();
-		int distance=0;
-		for(int i=0;i<loggedLocations.size()-1;i++) {
-			int possibleNewDistance=(int)loggedLocations.get(i).distance(current);
-			if (possibleNewDistance>distance) {
-				distance=possibleNewDistance;			
+		Location current = loggedLocations.getLast();
+		int distance = 0;
+		for(int i=0; i < loggedLocations.size()-1; i++) {
+			int possibleNewDistance = (int) loggedLocations.get(i).distance(current);
+			if (possibleNewDistance > distance) {
+				distance = possibleNewDistance;
 			}
 		}
 		return distance;
+	}
+
+	public BoundResults evaluateBounds(double relaxFactor) {
+		ActivityBounds newBounds = new ActivityBounds(loggedLocations);
+		if (lastBounds == null) {
+			lastBounds = newBounds;
+			return null;
+		} else {
+			return new BoundResults(
+				newBounds.contains(lastBounds) || lastBound.contains(newBounds),
+				newBounds.containsExcludeY(lastBounds) ||
+				lastBounds.containsExcludeY(newBounds),
+				newBounds.volume() * relaxFactor >= lastBounds.volume() ||
+				lastBounds.volume() * relaxFactor >= newBounds.volume(),
+				newBounds.travelSurface() * relaxFactor >= lastBounds.travelSurface() ||
+				lastBounds.travelSurface() * relaxFactor >= newBounds.travelSurface(),
+				newBounds.contains(lastBounds, relaxFactor) ||
+				lastBounds.contains(newBounds, relaxFactor),
+				newBounds.containsExcludeY(lastBounds, relaxFactor) ||
+				lastBounds.containsExcludeY(newBounds, relaxFactor));
+		}	
 	}
 }
